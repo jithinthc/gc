bb92129971f121bb01a277518a63edf6ea9d9bf3
Jithin Thomas <jithingoldeneye@gmail.com>
2020-12-19 07:55:49 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$('.owl-1').owlCarousel({
+    center: true,
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.owl-2').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
